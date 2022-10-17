f676f4b1f8c279f62c868518f308aaa77ed60a28
Rizki Agung Sentosa <mrrizkiagung@gmail.com>
2022-10-17 12:21:45 +0700

    Project sederhana Java-formbuysell-order

---
+++ form-sederhana-buysell-order-java/src/form/sederhana/pembayaran/barang/FormSederhanaPembayaranBarang.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package form.sederhana.pembayaran.barang;
+
+/**
+ *
+ * @author Ardy Sendleep
+ */
+public class FormSederhanaPembayaranBarang {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Frame_pembayaran_barang Laund = new Frame_pembayaran_barang();
+        Laund.setVisible(true);
+    }
+}
